3b796afc7c63b0e08b853e0432c028472e924c0c
saad altaf <saad.tfcl@gmail.com>
2022-12-15 17:30:58 +0500

    Changes in Application App Service

--- src/TFCLPortal.Application/Applications/ApplicationAppService.cs
+++ src/TFCLPortal.Application/Applications/ApplicationAppService.cs
@@ -932,7 +932,7 @@ namespace TFCLPortal.Applications
         {
             try
             {
-                var apps = GetApplicationList(applicationState, 0);
+                var apps = GetApplicationList(applicationState, 0,true,true,true);
 
                 if (apps.Count > 0)
                 {
