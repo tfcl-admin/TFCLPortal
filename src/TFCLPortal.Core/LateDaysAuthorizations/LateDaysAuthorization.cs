2e1ffc8f007ef1fde5ab27d749fbc36141664b08
Xainulabdin <zainulabdin58@gmail.com>
2023-07-19 12:20:06 +0500

    Name change of repository

--- src/TFCLPortal.Core/LateDaysAuthorizations/LateDaysAuthorization.cs
+++ src/TFCLPortal.Core/LateDaysAuthorizations/LateDaysAuthorization.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
-namespace TFCLPortal.LatePaymentAuthorization
+namespace TFCLPortal.LateDaysAuthorizations
 {
-    [Table("LatePaymentAuthorization")]
-    public class LatePaymentAuthorization : FullAuditedEntity<int>
+    [Table("LateDaysAuthorization")]
+    public class LateDaysAuthorization : FullAuditedEntity<int>
     {
         public int ApplicationId { get; set; }
         public int LateDays { get; set; }
